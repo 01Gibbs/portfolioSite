81bdfc9d0095b5185279a2e020db65168b1141e1
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-05-22 12:15:50 +0100

    AboutPage layout, setStates and Hooks, FramerMotion and placeholderText

---
+++ src/pages/about.jsx
@@ -0,0 +1,107 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/profilePic.png";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Daniel Gilbert | About</title>
+        <meta name="description" content="About the Programmer SEO" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose" className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16 ">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lr font-bold uppercase text-dark/75 ">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hello, my name is Daniel Gilbert, but please call me Gibbs! I am
+                a web developer with passion for innovation, and creating
+                user-centric digital experiences. I am always looking for new
+                ways to bring my clients' visions to fruition.
+              </p>
+              <p className="font-medium my-4">
+                Design is more than User Interface. At the core, it is solving
+                problems and creating intuitive user experiences that are
+                enjoyable and purposeful.
+              </p>
+              <p className="font-medium">
+                Whether I am working on a website, or a digital application, I
+                bring commitment to design excellence and user-centred thinking
+                to every project. I am excited for the opportunity to bring my
+                skills and passion to your project.
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 ">
+              <Image
+                src={profilePic}
+                alt="Daniel Gilbert"
+                className="w-full h-auto rounded-2xl"
+              />
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={50} />
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  satisfied clients
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={40} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={4} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  months of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
