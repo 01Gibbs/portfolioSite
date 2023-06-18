e737906687d93c10039f5fd7a080ed2ec293dba6
Gibbs <danielgilbert49@hotmail.co.uk>
2023-06-18 12:22:20 +0100

    Implementing Dark Mode Feature

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -6,11 +6,13 @@ const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
       <div className="w-48 h-auto flex items-center justify-center relative">
-        <CircularText className={"fill-dark animate-spin-slow"} />
+        <CircularText
+          className={"fill-dark animate-spin-slow dark:fill-light"}
+        />
 
         <Link
           href="mailto:danielgilbert49@hotmail.co.uk"
-          className="flex w-20 h-20 font-semibold rounded-full items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark hover:bg-light text-light hover:text-dark shadow-md border border-solid border-dark"
+          className="flex w-20 h-20 font-semibold rounded-full items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark dark:bg-light hover:bg-light hover:dark:bg-dark text-light dark:text-dark hover:text-dark hover:dark:text-light shadow-md border border-solid border-dark dark:border-light"
         >
           Hire Me
         </Link>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,13 @@ import {
   DribbbleIcon,
   GithubIcon,
   LinkedInIcon,
+  MoonIcon,
   PinterestIcon,
+  SunIcon,
   TwitterIcon,
 } from "./Icons";
 import { motion } from "framer-motion";
+import useThemeSwitch from "./hooks/useThemeSwitch";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -19,9 +22,8 @@ const CustomLink = ({ href, title, className = "" }) => {
     <Link href={href} className={`${className} relative group`}>
       {title}
       <span
-        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
-          router.asPath === href ? "w-full" : " w-0"
-        }`}
+        className={`h-[1px] inline-block bg-dark  dark:bg-light absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 
+        ${router.asPath === href ? "w-full" : " w-0"}`}
       >
         &nbsp;
       </span>
@@ -30,8 +32,10 @@ const CustomLink = ({ href, title, className = "" }) => {
 };
 
 const NavBar = () => {
+  const [mode, setMode] = useThemeSwitch();
+
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
       <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
         <CustomLink href="/about" title="About" className="mx-4" />
@@ -85,6 +89,18 @@ const NavBar = () => {
         >
           <DribbbleIcon />
         </motion.a>
+
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={`ml-3 flex items-center justify-center rounded-full p-1 
+          ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
+        >
+          {mode === "dark" ? (
+            <SunIcon className={"fill-dark"} />
+          ) : (
+            <MoonIcon className={"fill-dark"} />
+          )}
+        </button>
       </nav>
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
