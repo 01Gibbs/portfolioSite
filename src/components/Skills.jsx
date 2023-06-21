c4bd3df93482f50034d0a492af340092f7064e6f
Gibbs <danielgilbert49@hotmail.co.uk>
2023-06-21 16:16:56 +0100

    media query optimisation complete

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Skill = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark dark:bg-light text-light dark:text-dark py-3 px-6 shadow-dark dark:shadow-light cursor-pointer absolute"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark dark:bg-light text-light dark:text-dark py-3 px-6 shadow-dark dark:shadow-light cursor-pointer absolute lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:font-bold"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 2 } }}
@@ -18,10 +18,16 @@ const Skill = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark">
+      <h2 className="font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32">
+        Skills
+      </h2>
+      <div
+        className="w-full h-screen relative flex items-center justify-center rounded-full 
+      bg-circularLight lg:circularLightLG dark:bg-circularDark lg:dark:bg-circularDarkLg md:bg-circularLightMd md:dark:bg-circularDarkMd sm:bg-circularLightSm sm:dark:bg-circularLightSm
+      lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]"
+      >
         <motion.div
-          className="flex items-center justify-center rounded-full font-semibold bg-dark dark:bg-light text-light dark:text-dark p-8 shadow-dark dark:shadow-light cursor-pointer"
+          className="flex items-center justify-center rounded-full font-semibold bg-dark dark:bg-light text-light dark:text-dark p-8 shadow-dark dark:shadow-light cursor-pointer lg:p-6 md:p-4 xs:text-xs xs:p-2"
           whileHover={{ scale: 1.05 }}
         >
           Web
@@ -39,9 +45,9 @@ const Skills = () => {
         <Skill name="GraphQL" x="32vw" y="-5vw" />
         <Skill name="Express" x="-32vw" y="5vw" />
         <Skill name="Jest & Supertest" x="-20vw" y="7vw" />
-        <Skill name="GatsbyJS" x="0vw" y="-20vw" />
+        <Skill name="GatsbyJS" x="0vw" y="-35vw" />
         <Skill name="Tailwind CSS" x="0vw" y="-20vw" />
-        <Skill name="Firebase" x="0vw" y="-20vw" />
+        <Skill name="Firebase" x="0vw" y="-27.5vw" />
         <Skill name="PostgreSQL" x="-25vw" y="18vw" />
         <Skill name="MongoDB" x="25vw" y="-18vw" />
         <Skill name="Web Design" x="25vw" y="18vw" />
